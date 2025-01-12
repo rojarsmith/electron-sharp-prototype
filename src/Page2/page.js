d32ce175d40be9687179dfcc939a2a39c83418d0
Rojar Smith <rojarsmith@gmail.com>
2025-01-12 15:23:16 +0800

    feat: Add Breath Light

--- src/Page2/page.js
+++ src/Page2/page.js
@@ -1,10 +1,17 @@
 import '../App.css';
+import Home from './home.png'
 
 function Page() {
   return (
     <div>
       <div className={`page2-container`}>
-        <div>Page2</div>
+        <div class="center">
+          <div class="icon-container">
+            <img src={Home} alt="Home Icon" class="house-icon" />
+            <div class="consumption-text">自家消費率</div>
+            <div class="breathing-light"></div>
+          </div>
+        </div>
       </div>
     </div>
   );
